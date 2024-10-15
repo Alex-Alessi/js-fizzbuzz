ad76197ad4982ba2b5be382b7f3ae07afede7d77
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-15 16:06:27 +0200

    work complete

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-//! TITOLO
+console.log("JS Collegato");
+
+//! TiTOLO
 //* FizzBuzz
 
-//! TRACCIA
+//! TRACCiA
 // Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-//! RACCOLTA DATI
+//! RACCOLTA DATi
 
-//! ELABORAZIONE
+//! ELABORAZiONE
 
-//! OUTPUT
+//* creiamo un ciclo che vada da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  //* creiamo una variabile message che verrà sovrascritta nel caso in cui le condizioni vengano rispettate
+  let message = "";
+  //* controlliamo SE l'indice attuale è un multiplo di 3
+  if (i % 3 === 0) {
+    //* se lo è cambiamo il valore del messaggio con "Fizz" e controlliamo SE è anche multiplo di 5
+    message = "Fizz";
+    if (i % 5 === 0) {
+      //* SE è anche multiplo di 5, cambiamo il valore del messaggio con "FizzBuzz"
+      message = "FizzBuzz";
+    }
+  }
+
+  //* ALTRIMENTI controlliamo SE è solo un multiplo di 5
+  else if (i % 5 === 0) {
+    //* se lo è stampiamo "Buzz" al posto del numero
+    message = "Buzz";
+  }
+
+  //* ALTRIMENTI restituiamo il numero
+  else {
+    message = i;
+  }
+
+  //! OUTPUT
+  console.log(message);
+}
